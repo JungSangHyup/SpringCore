c69fa350990c6adc8a0253ae1481bad5839be724
SangHyup <51068026+JungSangHyup@users.noreply.github.com>
2021-10-31 14:27:50 +0900

    주문, 할인 객체

--- src/test/java/hello/core/member/MemberServiceImplTest.java
+++ src/test/java/hello/core/member/MemberServiceImplTest.java
@@ -10,14 +10,19 @@ class MemberServiceImplTest {
     
     @Test
     void join() {
+        //given
         Member member = new Member(1L, "memberA", Grade.VIP);
-        
+
+        //when
         memberService.join(member);
         Member findMember = memberService.findMember(1L);
+
+        //then
         Assertions.assertThat(member).isEqualTo(findMember);
     }
 
     @Test
-    void findMember() {
+    public Member findMember(Long memberId) {
+        return memberService.findMember(memberId);
     }
 }
